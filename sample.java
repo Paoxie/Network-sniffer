8ac4fc4238633c7b198c12dc4fe70f0e8e8be091
Paoxie <viqmsl@hotmail.com>
2013-09-01 00:33:54 +0200

    hello world

--- sample.java
+++ sample.java
@@ -1,42 +1,102 @@
+// for capturing
 import jpcap.JpcapCaptor;
 import jpcap.JpcapSender;
 import jpcap.NetworkInterfaceAddress;
 import jpcap.NetworkInterface;
 import jpcap.PacketReceiver;
+
+// just to include the packets required to sniff..
 import jpcap.packet.Packet;
 import jpcap.packet.ARPPacket;
 import jpcap.packet.ICMPPacket;
-import java.net.InetAddress;
-import java.io.IOException;
+
+// for charsetdeocding of packet
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.CharsetDecoder;
+import java.nio.ByteBuffer;
+
+// for reading packets
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// for writing packets
 import java.io.FileWriter;
+
+// use if necessary
+import java.io.IOException;
 
 public class sample {
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws Exception {
 
-	// for capturing
+	// open wifisniffer
 	int index = 2;
 	NetworkInterface[] devices = JpcapCaptor.getDeviceList();
 	JpcapCaptor captor = JpcapCaptor.openDevice(devices[index], 4096, false, 5000);
-	Packet pack = captor.getPacket();
 
-	// for sending
-	byte[] senderaddr = new byte[4];
-	ICMPPacket icmp = new ICMPPacket();
-	JpcapSender sender = captor.getJpcapSenderInstance();
+	// for decoding bytes captured
+	
+	ByteBuffer packbbheader = null;
+	ByteBuffer packbbdata = null;
+	CharBuffer charbuff = null;
+	CharsetDecoder csiso8859decoder = Charset.forName("ISO-8859-1").newDecoder();
+	CharsetDecoder csusasciidecoder = Charset.forName("US-ASCII").newDecoder();
+	CharsetDecoder csutf8decoder = Charset.forName("UTF-8").newDecoder();
+	CharsetDecoder csutf16bedecoder = Charset.forName("UTF-16BE").newDecoder();
+	CharsetDecoder csutf16ledecoder = Charset.forName("UTF-16LE").newDecoder();
+	CharsetDecoder csutf16decoder = Charset.forName("UTF-16").newDecoder();
+	Charset iso8859 = StandardCharsets.ISO_8859_1;
+	Charset us_ascii = StandardCharsets.US_ASCII;
+	Charset utf_16 = StandardCharsets.UTF_16;
+	Charset utf_16be = StandardCharsets.UTF_16BE;
+	Charset utf_16le = StandardCharsets.UTF_16LE;
+	Charset utf_8 = StandardCharsets.UTF_8;
+	Charset[] stdcs = {iso8859, us_ascii, utf_16, utf_16, utf_16be, utf_16le, utf_8};
+	int i = -1;
 
+	String packdata = new String();
+	String packheader = new String();
+	CharBuffer packcbdata = null;
+	CharBuffer packcbheader = null;
+	/*
+	FileWriter dumpwrite = new FileWriter(args[0]);
+	BufferedReader inread = new BufferedReader(new InputStreamReader(System.in));
+	*/
+	boolean run = true;
+	Packet pack = new Packet();
+	pack = captor.getPacket();
 	try {
-	    System.out.println("try catch block");
-	    capture(captor, pack, true, args[0]);
-	} catch (IOException e) {
-	    System.out.println("IOException");
-	    System.err.println(e);
-	}
+	    while (run) {
+		if (pack != null) {
+		    packbbheader = ByteBuffer.allocate(pack.header.length);
+		    packbbdata = ByteBuffer.allocate(pack.data.length);
+		    packbbheader.put(pack.header);
+		    packbbdata.put(pack.data);
+		    for (i = 0; i < stdcs.length; i++) {
+			packcbheader = stdcs[i].decode(packbbheader);
+			System.out.println("break");
+			System.out.println(packcbheader);
+		    }
+		    /*
+		      System.out.println(packheader);
+		      System.out.println();
+		      System.out.println(packdata);
+		      System.out.println();
+		      System.out.println();
+		    */
+		}
+	    }
+	} catch (Exception e) {
+	    System.out.println("Exception");
+	    e.printStackTrace();
+	}	
     }
 
-    public static void getSenderTarget(JpcapCaptor captor) throws IOException {
 
+    public static void getSenderTarget(JpcapCaptor captor) throws IOException {
 	captor.setFilter("arp", true);
 	ARPPacket arppack = (ARPPacket)captor.getPacket();
 	System.out.println("Sender IP Address:\t" + arppack.getSenderProtocolAddress());
@@ -46,27 +106,9 @@ public class sample {
 
     }
 
-    public static void capture(JpcapCaptor captor, Packet pack, boolean write, String file) throws IOException {
-	pack = captor.getPacket();
-	if (pack != null) {
-	    if (write == true) {
-		FileWriter dumpwrite = new FileWriter(file);
-		char[] c = new char[pack.data.length];
-		String packdata = new String(pack.data, "UTF-8");
-		String packheader = new String(pack.header, "UTF-8");
-		System.out.println(packdata);
-		System.out.println(packheader);
-		dumpwrite.write(packheader, 0, packheader.length());
-		dumpwrite.write("\n\n", 0, "\n\n".length());
-		dumpwrite.write(packdata, 0, packdata.length());
-		dumpwrite.write("\n", 0, "\n".length());
-		dumpwrite.close();
-	    }
-	}
-    }
-
-    public static void getdevs(NetworkInterface[] devices) throws IOException {
+    public static void getdevs() throws IOException {
 	//for each network interface
+	NetworkInterface[] devices = JpcapCaptor.getDeviceList();
 	for (int i = 0; i < devices.length; i++) {
 	    //print out its name and description
 	    System.out.println(i + ": " + devices[i].name + "(" + devices[i].description + ")");
